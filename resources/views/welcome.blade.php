14afac863044532fe382a4a7ee383c4d2946459d
koh <kohh4915@gmail.com>
2020-03-13 22:59:06 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-
+    @if(Auth::check())
+        {{Auth::user()->name}}
+    @else
         <div class="text-center" style="margin-top: 250px;">
             <h1>Sign up</h1>
             <p style>背景画像</p>
              {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
-        
+    @endif
 @endsection
 
