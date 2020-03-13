44fcd3652bf24fd42e8dc46c3c26f7de190709c6
koh <kohh4915@gmail.com>
2020-03-13 22:38:04 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
 
         <div class="text-center" style="margin-top: 250px;">
             <h1>Sign up</h1>
-            <p>背景画像</p>
+            <p style>背景画像</p>
+             {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
         
 @endsection
