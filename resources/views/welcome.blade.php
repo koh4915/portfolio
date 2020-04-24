a6bf61741c02062dbbc3038c79321814d421b36f
koh <kohh4915@gmail.com>
2020-04-24 23:52:34 +0000

    sixth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,8 +17,7 @@
         <div class="text-center" style="margin-top: 250px;">
              <h1>{!! link_to_route('signup.get', 'Sign up', [], ['style' => 'color:white;' , 'class' => 'nav-link']) !!}</h1>
         </div>
+        <div class="loginpass">Email : test@test.test , Password : test123</div>
     @endif
-    
-    <div class="loginpass">Email : test@test.test , Password : test123</div>
 @endsection
 
